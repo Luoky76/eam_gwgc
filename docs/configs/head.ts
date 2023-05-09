2189cdeeed890c6adcd653deee6c95fde8355c3f
hongyl <hongyl@xmeport.cn>
2023-05-09 19:05:13 +0800

    框架初始脚手架

--- docs/configs/head.ts
+++
@@ -1,5 +0,0 @@
-import type { HeadConfig } from '@vuepress/core'
-
-export const head: HeadConfig[] = [
-  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-]
--- docs/configs/navbar/zh.ts
+++
@@ -1,8 +0,0 @@
-import type { NavbarConfig } from '@vuepress/theme-default'
-
-export const navbarZh: NavbarConfig = [
-  {
-    text: '源码',
-    link: 'http://git.gksyb.com:3000/gksyb/framework-sample/',
-  }
-]
