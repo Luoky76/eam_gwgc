0e5e476ef641003a4833a95006d1078e0588e398
hongyl <hongyl@xmeport.cn>
2023-05-12 16:26:44 +0800

    job拆分

--- Gksyb.Server/Job/CleanLogJob.cs
+++ Gksyb.Server/Job/CleanLogJob.cs
@@ -5,14 +5,14 @@ using Quartz;
 namespace Gksyb.Server.Job
 {
     /// <summary>
-    /// 定时清理数据库
+    /// 定时清理数据库日志
     /// </summary>
-    public class CleanDatabaseJob : BaseJob
+    public class CleanLogJob : BaseJob
     {
         private readonly IDbContext _dbContext;
         private readonly DateTime _excuteDate = DateTime.Now;
 
-        public CleanDatabaseJob(IDbContext dbContext, ILogger<CleanDatabaseJob> logger) : base(logger, nameof(CleanDatabaseJob))
+        public CleanLogJob(IDbContext dbContext, ILogger<CleanLogJob> logger) : base(logger, nameof(CleanLogJob))
         {
             _dbContext = dbContext;
         }
@@ -22,7 +22,6 @@ namespace Gksyb.Server.Job
             _dbContext.DisableSqlLog();
             _dbContext.Session.CommandTimeout = 10 * 60;
             var days = _quartzTask.TaskData.CastTo(6 * 30) * -1;
-            await DeleteSysMessageAsync(days);
             await DeleteSysLogAsync(days);
         }
 
@@ -39,25 +38,6 @@ namespace Gksyb.Server.Job
             {
                 var next = date.AddDays(1);
                 await _dbContext.DeleteAsync<SYS_LOG>(c => c.LOGDATE >= date && c.LOGDATE <= next);
-                date = next;
-                if (_excuteDate.AddHours(4) < DateTime.Now) return;
-                await Task.Delay(500);
-            }
-        }
-
-        /// <summary>
-        /// 清理系统消息
-        /// </summary>
-        /// <returns></returns>
-        private async Task DeleteSysMessageAsync(int days)
-        {
-            var now = await _dbContext.GetSysdate();
-            var retain = now.Value.AddDays(days);
-            var date = await _dbContext.Query<SYS_MESSAGE>().MinAsync(c => c.CREATEDATE) ?? now.Value;
-            while (date < retain)
-            {
-                var next = date.AddDays(1);
-                await _dbContext.DeleteAsync<SYS_MESSAGE>(c => c.CREATEDATE >= date && c.CREATEDATE <= next);
                 date = next;
                 if (_excuteDate.AddHours(4) < DateTime.Now) return;
                 await Task.Delay(500);
---
+++ Gksyb.Server/Job/CleanMessageJob.cs
@@ -0,0 +1,47 @@
+﻿using Gksyb.Model.Core;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace Gksyb.Server.Job
+{
+    /// <summary>
+    /// 定时清理消息
+    /// </summary>
+    public class CleanMessageJob : BaseJob
+    {
+        private readonly IDbContext _dbContext;
+        private readonly DateTime _excuteDate = DateTime.Now;
+
+        public CleanMessageJob(IDbContext dbContext, ILogger<CleanMessageJob> logger) : base(logger, nameof(CleanMessageJob))
+        {
+            _dbContext = dbContext;
+        }
+
+        public override async Task Excute()
+        {
+            _dbContext.DisableSqlLog();
+            _dbContext.Session.CommandTimeout = 10 * 60;
+            var days = _quartzTask.TaskData.CastTo(6 * 30) * -1;
+            await DeleteSysMessageAsync(days);
+        }
+
+        /// <summary>
+        /// 清理系统消息
+        /// </summary>
+        /// <returns></returns>
+        private async Task DeleteSysMessageAsync(int days)
+        {
+            var now = await _dbContext.GetSysdate();
+            var retain = now.Value.AddDays(days);
+            var date = await _dbContext.Query<SYS_MESSAGE>().MinAsync(c => c.CREATEDATE) ?? now.Value;
+            while (date < retain)
+            {
+                var next = date.AddDays(1);
+                await _dbContext.DeleteAsync<SYS_MESSAGE>(c => c.CREATEDATE >= date && c.CREATEDATE <= next);
+                date = next;
+                if (_excuteDate.AddHours(4) < DateTime.Now) return;
+                await Task.Delay(500);
+            }
+        }
+    }
+}
