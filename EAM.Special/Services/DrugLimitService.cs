2d4147a8c1ea40acb09b3db868fff45f97f0fd89
Luoky <1085752693@qq.com>
2023-07-25 11:16:33 +0800

    添加药品管理页面

---
+++ EAM.Special/Services/DrugLimitService.cs
@@ -0,0 +1,153 @@
+﻿using Chloe;
+using EAM.Special.Interfaces;
+using Gksyb.Common;
+using Gksyb.Core.Application;
+using Gksyb.Core.Auth;
+using Gksyb.Core.Grid;
+using Gksyb.Core.Interfaces.Common;
+using Gksyb.Model;
+using Gksyb.Model.Grid;
+
+namespace EAM.Special.Services
+{
+    public class DrugLimitService : IDrugLimitService
+    {
+        private readonly IDbContext _dbContext;
+        private readonly IComboxDataService _comboxDataService;
+
+        public DrugLimitService(IDbContext dbContext, IComboxDataService comboxDataService)
+        {
+            _dbContext = dbContext;
+            _comboxDataService = comboxDataService;
+        }
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<GridData> ListAsync(GridRequest request)
+        {
+            var list = await _dbContext.Query<DRUG_LIMIT>().Select(c => new
+            {
+                c.LIMIT_ID,
+                c.SP_ID,
+                c.SP_CODE,
+                c.SP_NAME,
+                c.SP_TYPE,
+                c.UNIT,
+                c.INSIDE_APRIL,
+                c.OUTSIDE_APRIL,
+                c.INSIDE_OCTOBER,
+                c.OUTSIDE_OCTOBER,
+                c.CREATE_USERID,
+                c.CREATEDATE,
+                c.MODIFY_USERID,
+                c.MODIFYDATE
+            }).GetGridData(request);
+            return list;
+        }
+
+        /// <summary>
+        /// 根据ID获取单行记录
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<DRUG_LIMIT> GetAsync(string id)
+        {
+            var query = await _dbContext.Query<DRUG_LIMIT>().Where(c => c.LIMIT_ID == id).FirstAsync();
+            return query;
+        }
+
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<AjaxResult> SaveAsync(SaveRequest<DRUG_LIMIT> request)
+        {
+            return await _dbContext.SaveEntityAnsyc(request,
+                c => new
+                {
+                    c.LIMIT_ID,
+                    c.SP_ID,
+                    c.SP_CODE,
+                    c.SP_NAME,
+                    c.SP_TYPE,
+                    c.UNIT,
+                    c.INSIDE_APRIL,
+                    c.OUTSIDE_APRIL,
+                    c.INSIDE_OCTOBER,
+                    c.OUTSIDE_OCTOBER,
+                    c.CREATE_USERID,
+                    c.CREATEDATE,
+                    c.MODIFY_USERID,
+                    c.MODIFYDATE
+                },
+                c => a => a.LIMIT_ID == c.LIMIT_ID
+                , BeforeAdd, null, null, false, null, null);
+        }
+
+        /// <summary>
+        /// 添加前验证
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        private async Task BeforeAdd(DRUG_LIMIT entity)
+        {
+            if (entity.LIMIT_ID.IsNullOrEmpty())
+            {
+                entity.LIMIT_ID = GuidHelper.NewSnowflakeId().ToString();
+            }
+
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 更新前验证
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        private async Task BeforeUpdate(DRUG_LIMIT entity)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 删除前验证
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        private async Task BeforeDelete(DRUG_LIMIT entity)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 保存后验证
+        /// </summary>
+        private async Task AfterSave(List<DRUG_LIMIT> added, List<DRUG_LIMIT> updated, List<DRUG_LIMIT> deleted)
+        {
+            await Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// 获取下拉框数据
+        /// </summary>
+        public async Task<AjaxResult> ComboxData()
+        {
+            try
+            {
+                var data = await _comboxDataService.Get(new Dictionary<string, object>(){
+
+                });
+
+                return AjaxResult.Success(data);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("获取下拉数据失败！原因：" + e.Message);
+            }
+        }
+    }
+}
