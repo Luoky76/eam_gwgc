f14a9e1a1c0a6dcf4ebd9c492266fd292c13ca33
Luoky <1085752693@qq.com>
2023-08-25 08:48:02 +0800

    添加常规物料清点

---
+++ EAM.Special/Interfaces/ILaborMaterialDetService.cs
@@ -0,0 +1,42 @@
+﻿using Gksyb.Common;
+using Gksyb.Model;
+using Gksyb.Model.Grid;
+
+namespace EAM.Special.Interfaces
+{
+    public interface ILaborMaterialDetService : IService
+    {
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<GridData> ListAsync(GridRequest request);
+
+        /// <summary>
+        /// 根据ID获取单行记录
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<LABOR_MATERIAL_DET> GetAsync(string id);
+
+        /// <summary>
+        /// 生成主键
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        public string CreatePrimaryKey();
+
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public Task<AjaxResult> SaveAsync(SaveRequest<LABOR_MATERIAL_DET> request);
+
+        /// <summary>
+        /// 获取下拉框数据
+        /// </summary>
+        public Task<AjaxResult> ComboxData();
+    }
+}
