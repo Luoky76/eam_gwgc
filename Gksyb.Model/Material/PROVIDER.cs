1162a15a7c20eca0018898e44972161663ce6107
Luoky <1085752693@qq.com>
2023-10-10 17:39:34 +0800

    供应商信息登记bug

--- Gksyb.Model/Material/PROVIDER.cs
+++ Gksyb.Model/Material/PROVIDER.cs
@@ -84,7 +84,7 @@ namespace Gksyb.Model
         /// 注册资金
         /// </summary>
         [Description("注册资金")]
-        public decimal ENTITY_REG_FUND { get; set; }
+        public decimal? ENTITY_REG_FUND { get; set; }
 
         /// <summary>
         /// 企业性质
