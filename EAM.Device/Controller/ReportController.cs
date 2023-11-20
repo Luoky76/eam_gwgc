49f367f2fb85013f2015355d67c0113806d0d142
zhangly <2980800977@qq.com>
2023-11-20 16:03:51 +0800

    单船成本统计

---
+++ EAM.Device/Controller/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using EAM.Device.interfaces;
+using Gksyb.Common;
+using Gksyb.Core.Auth;
+using Gksyb.Model;
+using Gksyb.Model.Grid;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EAM.Device.controller
+{
+    [GksybAuthorize(true)]
+    public class ReportController : AreaController
+    {
+        private readonly IReportService _service;
+
+        public ReportController(IReportService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// 单船成本统计
+        /// </summary>
+        /// <param name="dateFrom"></param>
+        /// <param name="dateTo"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<GridData> CostReportAsync(string dateFrom, string dateTo)
+        {
+            return await _service.CostReportAsync(dateFrom, dateTo);
+        }
+    }
+}
---
+++ EAM.Device/interfaces/IReportService.cs
@@ -0,0 +1,14 @@
+﻿using Gksyb.Common;
+using Gksyb.Model;
+using Gksyb.Model.Grid;
+using Gksyb.Model.UI;
+using System.Collections.Concurrent;
+
+namespace EAM.Device.interfaces
+{
+    public interface IReportService : IService
+    {
+
+        Task<GridData> CostReportAsync(string dateFrom, string dateTo);
+    }
+}
