a98b8d0e5631484575e461b1870aa98b637507bf
Luoky <1085752693@qq.com>
2023-12-20 20:05:40 +0800

    OA对接

--- EAM.Material/Services/SpCollectService.cs
+++ EAM.Material/Services/SpCollectService.cs
@@ -900,8 +900,8 @@ namespace EAM.Material.Services
                 }
             }
 
-            string attach = attachName.TrimEnd('|') + (string.IsNullOrEmpty(fileName) ? "" : "|" + fileName) + "$$$" 
-                + attachUrl.TrimEnd('|') + (string.IsNullOrEmpty(fileName) ? "" : "|" + webUrl+ fileUrl);
+            string attach = attachName + (string.IsNullOrEmpty(fileName) ? "" : fileName) + "$$$" 
+                + attachUrl + (string.IsNullOrEmpty(fileName) ? "" : webUrl+ fileUrl);
 
             var mainQuery = await _dbContext.Query<SP_COLLECT>(c => c.COLLECT_ID == collectId)
                 .Select(c => new
@@ -921,7 +921,7 @@ namespace EAM.Material.Services
             string url = _dbContext.Query<BC_CODE>().Where(c => c.CODE_TYPE == "OA接口地址").First().CODE_EN;
 
             OAHandle oa = new OAHandle(_dbContext);
-            string result = await oa.CreateFlow(url, "SJQS", "工作请示（采购）-" + _userSession.RealName, _userSession.Phone, _userSession.UserName, jsonData, "");
+            string result = await oa.CreateFlow(url, "SJQS", "工作请示（采购）-" + _userSession.RealName, _userSession.Phone, _userSession.UserName, jsonData, "{}");
             //OA返回结果：{"msg":"创建流程成功","code":"1162464","success":true,"url":"999"}
             await _dbContext.DBLog("OA创建流程返回结果", "", "案件审批流程创建" + "\n" + result, "");
 
