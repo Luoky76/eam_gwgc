e83f0083845ef609912822ef810554b5c634f2a6
hongyl <hongyl@xmeport.cn>
2024-01-04 11:07:19 +0800

    ui ligerbutton加入提示

--- WebHost/wwwroot/lib/eui/js/ligerButton.js
+++ WebHost/wwwroot/lib/eui/js/ligerButton.js
@@ -8,12 +8,13 @@
 
     $.ligerDefaults.Button = {
         debounce: 300,//防抖
-        width: null,
-        type: "",
-        text: '',
-        disabled: false,
-        onClick: null,
-        icon: null
+        width: null,//宽度
+        type: "",//类型 primary circle ghost large
+        text: '',//显示文本
+        tip: "",//提示信息
+        disabled: false,//不可用
+        onClick: null,//点击事件
+        icon: null//图标 "fa fa-search"
     };
 
     $.ligerMethos.Button = $.ligerMethos.Button || {};
