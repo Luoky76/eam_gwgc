0d3c42992c00aab1da281ecd9ac5dca4e2c21547
Luoky <1085752693@qq.com>
2024-05-19 10:32:50 +0800

    文件大小写问题修复

--- WebHost/wwwroot/lib/flow-boot.js
+++
@@ -1,4 +0,0 @@
-﻿; (function () {
-    document.write('<link href="' + bootPATH + 'eflow/eflow.css?ver=' + JsVersion + '" rel="stylesheet" type="text/css" />');
-    document.write('<script src="' + bootPATH + 'eflow/eflow.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
-})();
