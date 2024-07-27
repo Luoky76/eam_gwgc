9e1302f5a128856a97e271696065c3abb02817cd
hongyl <hongyl@xmeport.cn>
2024-07-27 13:19:54 +0800

    workflow 优化任务处理，支持不同流程多次调用

---
+++ Gksyb.Workflow/Services/Workflow/Dtos/FlowBpmnData.cs
@@ -0,0 +1,17 @@
+﻿using Gksyb.Workflow.Services.Workflow.Bpmn;
+
+namespace Gksyb.Workflow.Services.Workflow.Dtos
+{
+    public class FlowBpmnData
+    {
+        /// <summary>
+        /// 任务节点
+        /// </summary>
+        public List<BpmnNodeService> Nodes { get; set; } = new List<BpmnNodeService>();
+
+        /// <summary>
+        /// 变迁
+        /// </summary>
+        public List<BpmnSequenceFlowService> Sequences { get; set; } = new List<BpmnSequenceFlowService> { };
+    }
+}
