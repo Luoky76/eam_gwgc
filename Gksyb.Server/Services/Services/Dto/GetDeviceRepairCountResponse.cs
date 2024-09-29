6b76fd76f7ad5e1c69d4d422414cd34f85488c71
Luoky <1085752693@qq.com>
2024-09-29 15:26:53 +0800

    首页报错问题处理

--- Gksyb.Server/Services/Services/Dto/GetDeviceRepairCountResponse.cs
+++ Gksyb.Server/Services/Services/Dto/GetDeviceRepairCountResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Gksyb.Server.Services.Services.Dto
+﻿namespace Gksyb.Server.Services.Services.Dto
 {
     public class GetDeviceRepairCountResponse
     {
@@ -12,14 +6,12 @@ namespace Gksyb.Server.Services.Services.Dto
         /// 设备数量
         /// </summary>
         public int deviceCount { get; set; }
+
         /// <summary>
         /// 维修数量
         /// </summary>
         public int repairCount { get; set; }
-        /// <summary>
-        /// 外协维修数量
-        /// </summary>
-        public int outrepairCount { get; set; }
+
         /// <summary>
         /// 码头维修数量
         /// </summary>
--- Gksyb.Server/Services/Services/WelcomeService.cs
+++ Gksyb.Server/Services/Services/WelcomeService.cs
@@ -123,12 +123,9 @@ namespace Gksyb.Server.Services.Message
         /// <returns></returns>
         public async Task<GetDeviceRepairCountResponse> GetDeviceRepairCount(DateTime datetime)
         {
-
-            //DateTime startTime = new DateTime(datetime.Year, datetime.Month, 1);
             GetDeviceRepairCountResponse result = new GetDeviceRepairCountResponse();
             result.deviceCount = await _dbContext.Query<DEVICE_CARD>(x => x.TYPE_NAME == "船舶" && x.AUDITING == "0").CountAsync();
             result.repairCount = await _dbContext.Query<REP_PLAN_EXE>(x => x.DEAL_TYPE == "自修" && x.AUDITING_D == "0").CountAsync();
-            result.outrepairCount = await _dbContext.Query<REP_PLAN_EXE>(x => x.DEAL_TYPE == "外协" && x.AUDITING_D == "0").CountAsync();
             result.shiprepairCount = await _dbContext.Query<REP_DOCK_PLAN>(x => x.AUDITING_PLAN == "0").CountAsync();
 
             return result;
