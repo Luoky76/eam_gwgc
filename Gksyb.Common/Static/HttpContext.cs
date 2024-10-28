f1ccfe0669f68128dc2fdcaafc9bbaf397f2dbc0
hongyl <hongyl@xmeport.cn>
2024-10-28 09:21:58 +0800

    common 获取本地IP处理只有无线网的情况

--- Gksyb.Common/Static/HttpContext.cs
+++ Gksyb.Common/Static/HttpContext.cs
@@ -16,7 +16,7 @@ namespace Gksyb.Common.Static
         static HttpContext()
         {
             AddressList = new ReadOnlyCollection<string>(NetworkInterface.GetAllNetworkInterfaces()
-                .Where(c => c.OperationalStatus == OperationalStatus.Up && !c.Description.Contains("virtual", StringComparison.OrdinalIgnoreCase) && !c.Description.Contains("pseudo", StringComparison.OrdinalIgnoreCase))
+                .Where(c => c.OperationalStatus == OperationalStatus.Up && !c.Description.ToLower().Contains("virtual") && !c.Description.ToLower().Contains("pseudo"))
                 .SelectMany(p => p.GetIPProperties().UnicastAddresses)
                 .Where(p => p.Address.AddressFamily == AddressFamily.InterNetwork && !IPAddress.IsLoopback(p.Address))
                 .Select(c => c.Address.ToString()).Distinct().OrderBy(i => i).ToList());
