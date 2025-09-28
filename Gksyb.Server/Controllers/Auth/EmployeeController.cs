2877da22010b3ebcddca3bf7cdd5ca13c79c6ee7
hongyl <hongyl@xmeport.cn>
2025-09-28 18:20:44 +0800

    common 加入员工管理（用户管理树化），部门人员可以穿透管理公司人员

---
+++ Gksyb.Server/Controllers/Auth/EmployeeController.cs
@@ -0,0 +1,99 @@
+﻿#pragma warning disable CA1822 // 将成员标记为 static
+using Gksyb.Core.Auth;
+using Gksyb.Core.Interfaces.Common;
+using Gksyb.Model.Core;
+using Gksyb.Model.Grid;
+using Gksyb.Server.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gksyb.Server.Controllers.Auth
+{
+    [GksybAuthorize(MenuNo = "EmployeeManage")]
+    public class EmployeeController : BaseController
+    {
+        private readonly EmployeeService _employeeService;
+        private readonly UserService _service;
+        public EmployeeController(EmployeeService employeeService, UserService userService)
+        {
+            _employeeService = employeeService;
+            _employeeService.AddParentCompanys().Result();
+            _service = userService;
+        }
+
+        /// <summary>
+        /// 树形结构
+        /// </summary>
+
+        public async Task<AjaxResult> TreeAsync()
+        {
+            return await _employeeService.TreeAsync();
+        }
+
+        /// <summary>
+        /// 下拉数据
+        /// </summary>
+        /// <returns></returns>
+        public async Task<AjaxResult> ComboxData([FromServices] IBCCodeService codeService)
+        {
+            return AjaxResult.Success(new
+            {
+                role = (await _service.RoleData()).Rows,
+                corp = (await _service.CorpData()).Rows,
+                station = await codeService.Get("岗位")
+            });
+        }
+
+        /// <summary>
+        /// 角色数据
+        /// </summary>
+        /// <returns></returns>
+        public async Task<AjaxResult> RoleData(GridRequest request)
+        {
+            return AjaxResult.Success(await _service.RoleData(request));
+        }
+
+        /// <summary>
+        /// 公司数据
+        /// </summary>
+        /// <returns></returns>
+        public async Task<AjaxResult> CorpData(GridRequest request)
+        {
+            return AjaxResult.Success(await _service.CorpData(request));
+        }
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<AjaxResult<GridData>> ListAsync(UserRequest user, GridRequest request)
+        {
+            return AjaxResult<GridData>.Success(await _service.ListAsync(user, request));
+        }
+
+        /// <summary>
+        /// 初始化密码
+        /// </summary>
+        /// <returns></returns>
+        [JsToken]
+        public async Task<AjaxResult> DoInitPassword(long? id)
+        {
+            return AjaxResult.Success(await _service.DoInitPassword(id), default);
+        }
+
+        /// <summary>
+        /// 保存
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [JsToken]
+        public async Task<AjaxResult> SaveAsync(SaveRequest<UserRequest> request)
+        {
+            var result = await ValidSaveAsync(request);
+            if (result.IsError) return result;
+            return await _service.Save(request);
+        }
+
+    }
+}
---
+++ Gksyb.Server/Services/Auth/EmployeeService.cs
@@ -0,0 +1,76 @@
+﻿using Gksyb.Core.Auth;
+using Gksyb.Core.Interfaces.Auth;
+using Gksyb.Model.Core;
+using Microsoft.Extensions.Options;
+
+namespace Gksyb.Server.Services.Auth
+{
+    public class EmployeeService : IBaseService
+    {
+        private readonly IDbContext _dbContext;
+        private readonly UserSession _user;
+        private readonly SysContextOptions _options;
+        /// <summary>
+        /// 组织服务
+        /// </summary>
+        public EmployeeService(IDbContext dbContext, UserSession userSession, IOptions<SysContextOptions> options)
+        {
+            _dbContext = dbContext;
+            _user = userSession;
+            _options = options.Value;
+        }
+
+        /// <summary>
+        /// 获取树形结构
+        /// </summary>
+        public async Task<AjaxResult> TreeAsync()
+        {
+            var list = await CoprQuery().OrderBy(c => c.CORP_SORT).ThenBy(c => c.CORPID).ToListAsync();
+            var data = list.Select(c => new
+            {
+                ID = c.CORPID,
+                TEXT = c.CORP_SNAME,
+                PARENTID = (string.IsNullOrWhiteSpace(c.CORPPARENTID) || c.CORPPARENTID == "0") ? "ROOT" : c.CORPPARENTID,
+                c.CORP_PATH,
+                c.CLASSFLAG,
+                ICON = "fa fa-group"
+            }).ToList();
+            if (_user.IsAdmin)
+            {
+                data.Add(new { ID = "ROOT", TEXT = "组织结构", PARENTID = "", CORP_PATH = "", CLASSFLAG = "", ICON = "fa fa-sitemap" });
+            }
+            return AjaxResult.Success(data);
+        }
+
+        /// <summary>
+        /// 将用户所属的父公司对应的子公司加入公司列表
+        /// </summary>
+        /// <returns></returns>
+        public async Task AddParentCompanys()
+        {
+            if (_user.IsAdmin || _user.ParentCompany == null || _user.ParentCompany?.CorpID == _user.Corp.CorpID)
+            {
+                return;
+            }
+            var allCorps = await CoprQuery().Select(CorpInfoExtensions.SelectCorpInfo).ToListAsync();
+            _user.AllCorps.Add(_user.ParentCompany);
+            _user.AllCorps.AddRange(_user.ParentCompany.ChildCorp(allCorps));
+            _user.AllCorps = _user.AllCorps.DistinctBy(c => c.CorpID).OrderBy(c => c.CorpID).ToList();
+        }
+
+        /// <summary>
+        /// 过滤父公司的数据
+        /// </summary>
+        private IQuery<CF_CORP> CoprQuery()
+        {
+            var query = _dbContext.Query<CF_CORP>().Where(c => c.VALIDFLAG == "1");
+            if (!_user.IsAdmin)
+            {
+                var treeNode = _user.ParentCompany?.TreeNode;
+                query = query.Where(c => c.CORP_PATH.StartsWith(treeNode));
+            }
+            return query;
+        }
+
+    }
+}
