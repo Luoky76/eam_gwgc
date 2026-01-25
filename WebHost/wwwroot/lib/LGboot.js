f284a3be5aeb66026e9b46f8656707ecac9ba963
hongyl <hongyl@xmeport.cn>
2026-01-25 21:22:16 +0800

    common ui加入黑暗模式

--- WebHost/wwwroot/lib/LGboot.js
+++ WebHost/wwwroot/lib/LGboot.js
@@ -12,7 +12,7 @@
         } catch (err) {
         }
     }
-    function GetPath(js, doc) {
+    function getPath(js, doc) {
         var path = "/", ver = "";
         for (var i = 0, l = doc.scripts.length; i < l; i++) {
             var src = doc.scripts[i].src;
@@ -28,19 +28,76 @@
         }
         return { path: path, ver: ver };
     }
-    var jsPath = GetPath("LGboot.js", window.document);
+
+    var THEME_KEY = "preferences-theme", DARK_THEME = "eui/ligerui-dark.css", DARK_CLASS = "l-dark-mode";
+    function isDarkTheme() {
+        for (var i = 0, l = window.localStorage.length; i < l; i++) {
+            var key = window.localStorage.key(i);
+            if (key.endsWith(THEME_KEY)) {
+                return /dark/.test(window.localStorage.getItem(key))
+            }
+        }
+        return false;
+    }
+    var jsPath = getPath("LGboot.js", window.document);
     window.bootPATH = jsPath.path;
     window.JsVersion = jsVer || jsPath.ver || (new Date().toJSON()).substring(0, 10).replace(/-/g, "");
 
     //head
-    document.write('<link href="' + bootPATH + 'dist/css/normalize.css" rel="stylesheet" type="text/css" />');
-    document.write('<link href="' + bootPATH + 'dist/css/font-awesome.css" rel="stylesheet" type="text/css" />');
-    document.write('<script src="' + bootPATH + 'jquery/jquery.js" type="text/javascript"></sc' + 'ript>');
-    document.write('<link href="' + bootPATH + 'eui/ligerui.css?ver=' + JsVersion + '" rel="stylesheet" type="text/css" />');
-    document.write('<script src="' + bootPATH + 'eui/ligerui.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
-    document.write('<link href="' + bootPATH + 'css/ui.expand.css?ver=' + JsVersion + '" rel="stylesheet" type="text/css" />');
-    document.write('<script src="' + bootPATH + 'js/ui.expand.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
-    document.write('<script src="' + bootPATH + 'js/common.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
-    document.write('<script src="' + bootPATH + 'js/LG.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
-    document.write('<script src="' + bootPATH + 'lc.js?ver=' + JsVersion + '" type="text/javascript"></sc' + 'ript>');
+    function loadCSS(src, skipVersion) {
+        src = bootPATH + src;
+        if (skipVersion !== true) {
+            src += "?ver=" + JsVersion;
+        }
+        var link = document.createElement('link');
+        link.href = src;
+        link.rel = 'stylesheet';
+        link.type = 'text/css';
+        document.head.appendChild(link);
+    }
+
+    function loadJS(src, skipVersion) {
+        src = bootPATH + src;
+        if (skipVersion !== true) {
+            src += "?ver=" + JsVersion;
+        }
+        document.write('<script src="' + src + '" type="text/javascript"></script>');
+    }
+
+    loadCSS('dist/css/normalize.css', true);
+    loadCSS('dist/css/font-awesome.css', true);
+    loadJS('jquery/jquery.js', true);
+    loadCSS('eui/ligerui.css');
+    loadJS('eui/ligerui.js');
+    loadCSS('css/ui.expand.css');
+    loadJS('js/ui.expand.js');
+    loadJS('js/common.js');
+    loadJS('js/LG.js');
+    loadJS('lc.js');
+
+    if (isDarkTheme()) {
+        setTimeout(function () {
+            document.body.classList.add(DARK_CLASS);
+        }, 300);
+        loadCSS(DARK_THEME);
+    }
+
+    window.addEventListener('storage', function (event) {
+        if (!event.key.endsWith(THEME_KEY)) {
+            return;
+        }
+        var search = 'link[href*="' + DARK_THEME + '"]';
+        document.body.classList.remove(DARK_CLASS);
+        if (/dark/.test(event.newValue)) {
+            if (!document.querySelector(search)) {
+                document.body.classList.add(DARK_CLASS);
+                loadCSS(DARK_THEME);
+            }
+        } else {
+            var darkLink = document.querySelector(search);
+            if (darkLink) {
+                darkLink.parentNode.removeChild(darkLink);
+            }
+        }
+    });
 })();
