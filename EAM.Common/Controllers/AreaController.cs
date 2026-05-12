9ea8c07b312ae97cf93a93c21321371070ef4d61
Luoky <1085752693@qq.com>
2026-05-12 19:11:26 +0800

    添加通用主键生成方法

---
+++ EAM.Common/Controllers/AreaController.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.AspNetCore.Mvc
+{
+    [Route("Common")]
+    [ApiController]
+    public class AreaController<T> : BaseController<T>
+    {
+    }
+
+    [Route("Common")]
+    [ApiController]
+    public class AreaController : BaseController
+    {
+    }
+}
